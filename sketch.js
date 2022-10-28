7930f4c0dc0965dd6cc8183c1ff71bec2a6a9910
vanna567 <112666892+vanna567@users.noreply.github.com>
2022-10-27 20:18:31 -0500

    fuerza

--- sketch.js
+++ sketch.js
@@ -81,7 +81,7 @@ function draw() {
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
 
-		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+		Matter.Body.applyForce(ball,ball.position,{x:70,y:-35});
     
   	}
 }
